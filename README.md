eba0077b17aa3164d1bf5f1133abcdf4847dfa27
prajeshElEvEn <prajesh.eleven118@gmail.com>
2023-10-06 11:03:09 +0530

    updated README

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # Reactjs-MaterialUI-Resturant-website
-Complete Reactjs Resturant Website 
+
+Complete Reactjs Resturant Website
+
+## Installation
+
+- CLone the repository
+
+```bash
+git clone
+```
+
+- Navigate to directory
+
+```bash
+cd kishansweets
+```
+
+- Install dependencies
+
+```bash
+npm i
+```
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import Layout from '../components/Layout/Layout';
-import '../styles/Main.css';
-import { EmblaCarousel } from '../components/Layout/Banner';
-import MainAbout from '../components/Layout/MainAbout';
+import React from "react";
+import Layout from "../components/Layout/Layout";
+import "../styles/Main.css";
+import { EmblaCarousel } from "../components/Layout/Banner";
+import MainAbout from "../components/Layout/MainAbout";
 const Main = () => {
-	return (
-		<Layout>
-			<div className="main">
-				<EmblaCarousel />
-			</div>
-			<MainAbout />
-		</Layout>
-	);
+  return (
+    <Layout>
+      <div className="main">
+        <EmblaCarousel />
+      </div>
+      <MainAbout />
+    </Layout>
+  );
 };
 
 export default Main;
